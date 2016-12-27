80f7776ee9451fdc432efc332c2a11f25e5f91e7
edunavarro13 <edunavarro13@gmail.com>
2016-12-27 21:25:22 +0100

    Calculadora v2

--- calculadora.c
+++ calculadora.c
@@ -4,7 +4,25 @@ using namespace std;
 
 int main () 
 {
+	char opcion;
 	cout << "\n----------------------------------------" << endl;
 	cout << "------------- Calculadora --------------" << endl;
 	cout << "----------------------------------------" << endl;
+	cout << "1. Sumar." << endl;
+	cout << "2. Restar." << endl;
+	cout << "q. Salir." << endl;
+	cin >> opcion;
+
+	if (opcion == '1')
+	{
+		cout << "\nHas elegido suma." << endl;
+	}
+	else if(opcion == '2')
+	{
+		cout << "\nHas elegido resta." << endl;
+	}
+	else if(opcion != 'q')
+	{
+		cout << "\nLa opcion elegida no es una de las posibles." << endl;
+	}
 }
