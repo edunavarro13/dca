6a339585a90363516ef12abbd5b35a77eab3e8c2
edunavarro13 <edunavarro13@gmail.com>
2016-12-27 21:20:18 +0100

    Calculadora v1

---
+++ calculadora.c
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main () 
+{
+	cout << "\n----------------------------------------" << endl;
+	cout << "------------- Calculadora --------------" << endl;
+	cout << "----------------------------------------" << endl;
+}
