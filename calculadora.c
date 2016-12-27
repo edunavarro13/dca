58ef26deef3a4b04be20bc6a600883bf1c871bed
edunavarro13 <edunavarro13@gmail.com>
2016-12-27 21:31:57 +0100

    Calculadora v3

--- calculadora.c
+++ calculadora.c
@@ -5,24 +5,41 @@ using namespace std;
 int main () 
 {
 	char opcion;
-	cout << "\n----------------------------------------" << endl;
-	cout << "------------- Calculadora --------------" << endl;
-	cout << "----------------------------------------" << endl;
-	cout << "1. Sumar." << endl;
-	cout << "2. Restar." << endl;
-	cout << "q. Salir." << endl;
-	cin >> opcion;
+	bool salir = false;
+	do {
+		cout << "\n----------------------------------------" << endl;
+		cout << "------------- Calculadora --------------" << endl;
+		cout << "----------------------------------------" << endl;
+		cout << "1. Sumar." << endl;
+		cout << "2. Restar." << endl;
+		cout << "q. Salir." << endl;
+		cin >> opcion;
 
-	if (opcion == '1')
-	{
-		cout << "\nHas elegido suma." << endl;
-	}
-	else if(opcion == '2')
-	{
-		cout << "\nHas elegido resta." << endl;
-	}
-	else if(opcion != 'q')
-	{
-		cout << "\nLa opcion elegida no es una de las posibles." << endl;
-	}
+		if (opcion == '1')
+		{
+			cout << "\nHas elegido suma." << endl;
+		}
+		else if(opcion == '2')
+		{
+			cout << "\nHas elegido resta." << endl;
+		}
+		else if(opcion != 'q')
+		{
+			cout << "\nLa opcion elegida no es una de las posibles." << endl;
+		}
+		else
+		{
+			salir = true;
+		}
+
+		if(!salir)
+		{
+			char con;
+			cout << "¿Quiere hacer otra consulta? (y/n): ";
+			cin >> con;
+
+			if(con == 'n')
+				salir = true;
+		}
+	} while (!salir);
 }
